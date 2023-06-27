dbb12ab2010fb6812904fb8ad7fe833d2ebafbc3
Gui Silva <adrianon10@gmail.com>
2023-06-27 04:18:53 -0300

    chore: add first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  variable: '--font-poppins',
+})
+
+export const metadata = {
+  title: 'Beauty Salon',
+  description: 'Beauty Salon, o Salão de beleza cm hora marcada 😍',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#348789',
+        secondary: '#001489',
+        gray: {
+          50: '#DDDFEB',
+          100: '#97909050',
+          200: '#535653',
+          300: '#7C8388',
+        },
+      },
+      fontFamily: {
+        sans: 'var(--font-poppins)',
+      },
+    },
+  },
+  plugins: [],
+}
