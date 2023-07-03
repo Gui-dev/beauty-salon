a8c675b71dcff498cc0a790b7039614394f29055
Gui Silva <adrianon10@gmail.com>
2023-07-03 03:31:03 -0300

    feat: add isLoading property

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,21 @@
+import { ImSpinner3 } from 'react-icons/im'
+
 interface IButton {
   title: string
+  isLoading?: boolean
 }
 
-export const Button = ({ title }: IButton) => {
+export const Button = ({ title, isLoading = false }: IButton) => {
   return (
     <button
       type="submit"
       className="flex w-full items-center justify-center rounded-lg bg-secondary  px-9 py-3"
     >
-      <span className="font-sans text-lg font-bold text-white">{title}</span>
+      {isLoading ? (
+        <ImSpinner3 size={20} className="animate-spin text-white" />
+      ) : (
+        <span className="font-sans text-lg font-bold text-white">{title}</span>
+      )}
     </button>
   )
 }
