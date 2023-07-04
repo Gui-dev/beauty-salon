3b620debd9b2018b01fe895c3211eec5ab56b7f3
Gui Silva <adrianon10@gmail.com>
2023-07-04 03:53:58 -0300

    feat: add toastify styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { ToastContainer } from 'react-toastify'
+
 import { Login } from '@/components/Login'
 
 export default function Home() {
   return (
     <div className="">
+      <ToastContainer theme="dark" position="top-right" />
       <Login />
     </div>
   )
