c2aa78fc509b2b4d66b337435a901f83a4a93154
Gui Silva <adrianon10@gmail.com>
2023-07-14 03:12:42 -0300

    feat: add refresh token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { api } from './services/api'
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
-
-interface IRequestConfig extends AxiosRequestConfig {
-  onFailure?: (error: AxiosError) => void
-  onSuccess?: (response: AxiosResponse) => void
-}
-const refreshSubscribers: Array<(token: string) => void> = []
-let failedRequest: Array<IRequestConfig> = []
 
 export const middleware = (request: NextRequest) => {
   const token = request.cookies.get('beauty_token')?.value
@@ -16,55 +7,7 @@ export const middleware = (request: NextRequest) => {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
-  api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${token}`
-    return config
-  })
-
-  api.interceptors.response.use(
-    (response) => response,
-    async (error: AxiosError | unknown) => {
-      const originalRequest = (error as AxiosError).config as IRequestConfig
-      if (error instanceof AxiosError && error.response?.status === 401) {
-        if (
-          error.response.data &&
-          error.response.data.code === 'Authorization token expired'
-        ) {
-          try {
-            const refresh = request.cookies.get('beauty_refresh_token')?.value
-            const { data } = await api.post('/refresh', {
-              refresh_token: refresh,
-            })
-            const { token, refresh_token } = data
-            request.cookies.set('beauty_token', token)
-            request.cookies.set('beauty_refresh_token', refresh_token)
-            onRefreshed(token)
-            if (originalRequest?.headers) {
-              originalRequest.headers.Authorization = `Bearer: ${token}`
-            }
-            return axios(originalRequest)
-          } catch (error) {
-            failedRequest.forEach((request) => {
-              request.onFailure?.(error as AxiosError)
-            })
-            failedRequest = []
-          }
-        }
-      } else {
-        request.cookies.delete('beauty_token')
-        request.cookies.delete('beauty_refresh_token')
-        request.cookies.delete('beauty_user')
-      }
-
-      return Promise.reject(error)
-    },
-  )
-
   return NextResponse.next()
-}
-
-const onRefreshed = (token: string) => {
-  refreshSubscribers.forEach((callback) => callback(token))
 }
 
 export const config = {
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,63 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import cookies from 'js-cookie'
+
+interface IRequestConfig extends AxiosRequestConfig {
+  onFailure?: (error: AxiosError) => void
+  onSuccess?: (response: AxiosResponse) => void
+}
+
+const token = cookies.get('beauty_token')
 
 export const api = axios.create({
   baseURL: 'http://192.168.0.102:3333',
 })
+
+const refreshSubscribers: Array<(token: string) => void> = []
+let failedRequest: Array<IRequestConfig> = []
+
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError | unknown) => {
+    const originalRequest = (error as AxiosError).config as IRequestConfig
+    if (error instanceof AxiosError && error.response?.status === 401) {
+      if (
+        error.response.data &&
+        error.response.data.error === 'Token exprired'
+      ) {
+        try {
+          const refresh = cookies.get('beauty_refresh_token')
+          const { data } = await api.post('/users/refresh', {
+            refresh_token: refresh,
+          })
+          const { token, refresh_token } = data
+          cookies.set('beauty_refresh_token', refresh_token)
+          onRefreshed(token)
+          if (originalRequest?.headers) {
+            originalRequest.headers.Authorization = `Bearer: ${token}`
+          }
+          return axios(originalRequest)
+        } catch (error) {
+          failedRequest.forEach((request) => {
+            request.onFailure?.(error as AxiosError)
+          })
+          failedRequest = []
+        }
+      }
+    } else {
+      cookies.remove('beauty_token')
+      cookies.remove('beauty_refresh_token')
+      cookies.remove('beauty_user')
+    }
+
+    return Promise.reject(error)
+  },
+)
+
+const onRefreshed = (token: string) => {
+  refreshSubscribers.forEach((callback) => callback(token))
+}
