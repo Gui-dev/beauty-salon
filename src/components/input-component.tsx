7e9216ddd552f03f2b9877a5814b08cb53947abf
Gui Silva <adrianon10@gmail.com>
2023-07-21 03:36:35 -0300

    feat: add classnames to add more styles

--- src/components/input-component.tsx
+++ src/components/input-component.tsx
@@ -1,3 +1,4 @@
+import classnames from 'classnames'
 import {
   ElementType,
   ForwardRefRenderFunction,
@@ -7,8 +8,8 @@ import {
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
-  error: string | undefined
-  icon: ElementType
+  error?: string | undefined
+  icon?: ElementType
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInputProps> = (
@@ -18,11 +19,25 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInputProps> = (
   return (
     <div className="flex w-full flex-col items-center rounded-lg">
       <label htmlFor={label} className="relative flex w-full items-center">
-        <i aria-hidden="true" className="absolute pl-2">
-          <Icon size={20} />
-        </i>
+        {Icon && (
+          <i
+            aria-hidden="true"
+            className={classnames('absolute', {
+              'pl-2': Icon,
+            })}
+          >
+            <Icon size={20} />
+          </i>
+        )}
         <input
-          className="w-full rounded-lg border border-gray-50 bg-white px-9 py-3"
+          className={classnames(
+            'w-full rounded-lg border border-primary-900 bg-white py-3 text-gray-800 placeholder-primary-800',
+            {
+              'px-9': Icon,
+              'px-3': !Icon,
+              'border-red-300': error,
+            },
+          )}
           id={label}
           ref={ref}
           {...data}
