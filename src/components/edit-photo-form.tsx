0e95048cbe3b2b2f2bf4592b971195a6a144dca7
Gui Silva <adrianon10@gmail.com>
2023-07-25 05:09:48 -0300

    feat: add helper function handleUpdatePhoto

--- src/components/edit-photo-form.tsx
+++ src/components/edit-photo-form.tsx
@@ -1,32 +1,53 @@
 'use client'
 
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { FiEdit2 } from 'react-icons/fi'
-import userProfile from '@/assets/user.png'
+import cookies from 'js-cookie'
 
+import userProfile from '@/assets/user.png'
 import { useAuth } from '@/hooks/auth'
 import {
   EditPhotoValidationData,
   editPhotoValidation,
 } from '@/validation/edit-profile-validation'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import colors from 'tailwindcss/colors'
+import { toast } from 'react-toastify'
+import { api } from '@/services/api'
 
 export const EditPhotoForm = () => {
+  const router = useRouter()
   const { user } = useAuth()
-  const [fileUpload, setFileUpload] = useState(user?.avatar_url || userProfile)
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<EditPhotoValidationData>({
+  const [fileUpload, setFileUpload] = useState<string | StaticImageData>(
+    userProfile,
+  )
+  const { register, handleSubmit } = useForm<EditPhotoValidationData>({
     resolver: zodResolver(editPhotoValidation),
   })
 
   const handleUpdatePhoto = async (data: EditPhotoValidationData) => {
-    console.log('Foto', data.image?.[0])
+    if (data.image) {
+      try {
+        const image = data.image?.[0]
+        const imageUrl = URL.createObjectURL(image)
+        setFileUpload(imageUrl)
+        const fileToUpload = new FormData()
+        fileToUpload.set('avatar_url', image)
+        const result = await api.put('uploads', fileToUpload)
+        cookies.set('beauty_user', JSON.stringify(result.data))
+        toast.success('Avatar atualizado com sucesso')
+        setFileUpload(result.data.avatar_url)
+        router.push('/dashboard')
+      } catch (error) {
+        console.log(error)
+        toast.error('Erro ao tentar atualizar imagem')
+      }
+    } else {
+      toast.error('Você precisa selecionar uma imagem')
+    }
   }
 
   return (
@@ -34,11 +55,11 @@ export const EditPhotoForm = () => {
       {fileUpload && (
         <div className="relative">
           <Image
-            src={fileUpload}
+            src={user?.avatar_url ? user.avatar_url : fileUpload}
             alt={`Edite sua foto de perfil`}
             width={112}
             height={112}
-            className="h-20 w-20 rounded-full border border-solid border-primary-900"
+            className="h-28 w-28 rounded-full border-2 border-solid border-primary-900 bg-cover hover:border-primary-800"
           />
           <label
             htmlFor="image"
